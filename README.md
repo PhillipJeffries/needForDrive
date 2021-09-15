8fe134bd90543704245f90cfd37c8491dd1ffe80
PhillipJeffries <ggbox@mail.ru>
2021-09-15 13:13:02 +0400

    cleaned default app data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 create react app
+
+npm install --save-dev
+gh-pages, sass
---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import './App.scss';
+
+function App() {
+  return (
+    <h1>Need for drive</h1>
+  );
+}
+
+export default App;
