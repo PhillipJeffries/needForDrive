f437bc9082991d8d90dd2d6a09b8bf53ed34bfe1
PhillipJeffries <ggbox@mail.ru>
2021-09-17 15:59:32 +0400

    markup begining
    added fonts, customize.scss,
    basic markup

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ ghPages https://phillipjeffries.github.io/needfordrive/
 
 npm install --save-dev
 gh-pages, sass
+
+customize.scss (variables, mixins,)
+
+
+common markup:
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,15 @@ import './App.scss';
 
 function App() {
   return (
-    <h1>Need for drive</h1>
+    <div className="page-container">
+      <div className="side-menu"></div>
+      <div className="page-content container">
+        <header className="header"></header>
+        <section className="content-wrapper"></section>
+        <footer className="footer"></footer>
+      </div>
+      <div className="slider"></div>
+    </div>
   );
 }
 
