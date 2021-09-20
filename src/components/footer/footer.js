72e82b801bca3fced522b791fe1cb76e7922f0b9
PhillipJeffries <ggbox@mail.ru>
2021-09-20 19:54:25 +0400

    components
    header footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,10 @@ import './footer.scss';
 
 const Footer = () => {
     return(
-        <footer className="footer">footer</footer>
+        <footer className="footer">
+            <span>© 2016-2019 «Need for drive»</span>
+            <a href="tel:+74952342244"><span>8 (495) 234-22-44</span></a>
+        </footer>
     )
 }
 
--- src/components/sideMenu/sideMenu.js
+++ src/components/sideMenu/sideMenu.js
@@ -8,7 +8,13 @@ const SideMenu = () => {
             <div></div>
             <div></div>
             </button>
-            <button className="eng-button">Eng</button>
+            <div className="lang-button-wrapper">
+                <button className="lang-button">
+                    <div className="lang-button-frame"></div>
+                    Eng
+                </button>
+            </div>
+
         </div>
     )
 }
